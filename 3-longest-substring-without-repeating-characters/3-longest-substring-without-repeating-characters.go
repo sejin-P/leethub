4c9a7cbcdb2af02fa08127dc6e115d16449c9020
sejinP <42539506+sejin-P@users.noreply.github.com>
2022-03-23 23:10:05 +0900

    Time: 13 ms (48.89%), Space: 3 MB (67.83%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.go
@@ -0,0 +1,19 @@
+func lengthOfLongestSubstring(s string) int {
+    letterDict := map[uint8]int{}
+    result := 0
+    start := 0
+    for i := 0; i < len(s); i++ {
+        idx, ok := letterDict[s[i]]
+        if !ok || idx < start {
+            if i-start+1 > result {
+                result = i-start+1
+            }
+        } else {
+            start = idx + 1
+        }
+        
+        letterDict[s[i]] = i
+    }
+    
+    return result
+}
