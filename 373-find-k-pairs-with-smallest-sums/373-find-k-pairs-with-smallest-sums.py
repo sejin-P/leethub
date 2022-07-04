2c6e76caba21d2362762cadc7b75a73c5fb5600d
sejinP <42539506+sejin-P@users.noreply.github.com>
2022-07-04 23:18:55 +0900

    Time: 2222 ms (20.55%), Space: 34.2 MB (26.37%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.py
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.py
@@ -1,24 +1,21 @@
+from heapq import *
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        answer = []
-        heap = []
-        visited = set()
-        
-        i, j = 0, 0
-        visited.add((i,j))
-        heapq.heappush(heap, (nums1[i]+nums2[j], i, j))
-        
-        while k > 0 and heap:
-            
-            _, i, j = heapq.heappop(heap)
-            answer.append([nums1[i], nums2[j]])
-            
-            if i < len(nums1) - 1 and (i+1, j) not in visited:
-                heapq.heappush(heap, (nums1[i+1]+nums2[j], i+1, j))
-                visited.add((i+1, j))
-            if j < len(nums2) - 1 and (i, j+1) not in visited:
-                heapq.heappush(heap, (nums1[i]+nums2[j+1], i, j+1))
-                visited.add((i, j+1))
-            k -= 1
-        return answer
+        maxHeap = []
+        heapify(maxHeap)
+        for i in range(min(len(nums1), k)):
+            for j in range(min(len(nums2), k)):
+                pair = [nums1[i], nums2[j]]
+                if len(maxHeap) < k:
+                    heappush(maxHeap, [-1*(nums1[i] + nums2[j]), pair])
+                else:
+                    if nums1[i] + nums2[j] > -maxHeap[0][0]:
+                        break
+                    heappush(maxHeap, [-1*(nums1[i] + nums2[j]), pair])
+                    heappop(maxHeap)
+                    
+        pairs = [] 
+        for val in maxHeap:
+            pairs.append(val[1])
+        return pairs
         
