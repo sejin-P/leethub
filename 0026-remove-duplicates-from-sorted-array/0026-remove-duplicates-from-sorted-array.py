7f53f39725801568ca8ac8263661aefc475e1dcb
Sejin Park <42539506+sejin-P@users.noreply.github.com>
2024-04-07 23:09:04 +0900

    Time: 69 ms (87.55%), Space: 17.9 MB (91.13%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,14 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         cnt = 0
-        ret = []
-        unserscore_ret = []
         prev_num = -101
         for num in nums:
             if num != prev_num:
                 nums[cnt] = num
                 cnt += 1
-                ret.append(num)
                 prev_num = num
         
         return cnt
