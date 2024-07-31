471c6c5ab30d01010a3afbead8d1b2a58eda42bc
Sejin Park <42539506+sejin-P@users.noreply.github.com>
2024-08-01 00:50:18 +0900

    Time: 165 ms (34.11%), Space: 28.1 MB (20.99%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -12,7 +12,5 @@ class Solution:
             nums[(i+k)%n] = nums[i]
         
         for i in range(k, n):
-            n_idx = (i+k)%n
-            r_idx = i%k
-            nums[n_idx], r[r_idx] = r[r_idx], nums[n_idx]
+            nums[(i+k)%n], r[i%k] = r[i%k], nums[(i+k)%n]
         
