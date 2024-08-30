2c9ad6f6424f20eb5fdcfd31213431890dbd8522
Sejin Park <42539506+sejin-P@users.noreply.github.com>
2024-08-30 20:54:24 +0900

    Time: 52 ms (14.40%), Space: 17.1 MB (63.25%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,60 +3,17 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
         
-        left, right = 0, m-1
+        left, right = 0, m*n-1
         
-        while left < right:
-            mid = (left+right)//2
-            if matrix[left][0] < target and target < matrix[mid][0]:
-                right = mid
-                continue
-                
-            if matrix[mid][0] < target and target < matrix[right][0]:
-                if left == mid:
-                    break
-                left = mid 
-                continue
-                
-            if matrix[left][0] > target:
-                return False
-            
-            if matrix[right][0] < target:
-                left = right
-                
-            if matrix[left][0] == target:
+        while left <= right:
+            mid = (left+right) // 2
+            num = matrix[(mid // n)][mid%n]
+            if num == target:
                 return True
-            if matrix[mid][0] == target:
-                return True
-            if matrix[right][0] == target:
-                return True
+            if num < target:
+                left = mid + 1
+            if num > target:
+                right = mid - 1
         
-        row = left if matrix[right][0] > target else right
+        return False
         
-        left, right = 0, n-1
-        
-        while left< right:
-            mid = (left+right)//2
-            if matrix[row][left] < target and target < matrix[row][mid]:
-                right = mid
-                continue
-                
-            if matrix[row][mid] < target and target < matrix[row][right]:
-                if left == mid:
-                    break
-                left = mid 
-                continue
-                
-            if matrix[row][left] > target:
-                return False
-            
-            if matrix[row][right] < target:
-                left = right
-                
-            if matrix[row][left] == target:
-                return True
-            if matrix[row][mid] == target:
-                return True
-            if matrix[row][right] == target:
-                return True
-            
-        return matrix[row][left] == target or matrix[row][right] == target
