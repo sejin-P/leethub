cdb4504f5ed6cfa63713a664af05b4fc4e383c78
Sejin Park <42539506+sejin-P@users.noreply.github.com>
2024-09-17 23:24:04 +0900

    Time: 44 ms (97.37%), Space: 18.1 MB (94.14%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -7,25 +7,24 @@
 class Solution:
     def __init__(self):
         self.preorder_idx = 0
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        def arr_to_tree(left_idx, right_idx):
-            if left_idx > right_idx:
-                return None
-            
-            root_val = preorder[self.preorder_idx]
-            root = TreeNode(root_val)
-            
-            self.preorder_idx += 1
-            
-            root.left = arr_to_tree(left_idx, inorder_idx_map[root_val]-1)
-            root.right = arr_to_tree(inorder_idx_map[root_val]+1, right_idx)
-            
-            return root
-            
-        
-        inorder_idx_map = {}
+        self.inorder_idx_map = {}
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:       
         for i in range(len(inorder)):
-            inorder_idx_map[inorder[i]] = i
+            self.inorder_idx_map[inorder[i]] = i
             
-        return arr_to_tree(0, len(preorder)-1)
+        return self.arr_to_tree(0, len(preorder)-1, preorder)
+    def arr_to_tree(self, left_idx, right_idx, preorder):
+        if left_idx > right_idx:
+            return None
+        
+        root_val = preorder[self.preorder_idx]
+        root = TreeNode(root_val)
+        
+        self.preorder_idx += 1
+        
+        root.left = self.arr_to_tree(left_idx, self.inorder_idx_map[root_val]-1, preorder)
+        root.right = self.arr_to_tree(self.inorder_idx_map[root_val]+1, right_idx, preorder)
+        
+        return root
+        
         
