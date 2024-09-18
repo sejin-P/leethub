7c7991b817959fc503b030c5f814e14445615b21
Sejin Park <42539506+sejin-P@users.noreply.github.com>
2024-09-18 23:15:44 +0900

    Time: 113 ms (9.61%), Space: 20.8 MB (38.09%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.py
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.py
@@ -10,35 +10,38 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findMid(self, head: ListNode) -> ListNode:
-        prevNode = None
-        slowNode = head
-        fastNode = head
+    def __init__(self):
+        self.head = None
+    def size(self, head: Optional[ListNode]) -> int:
+        l = 0
+        ptr = head
+        while ptr:
+            l += 1
+            ptr = ptr.next
         
-        while fastNode and fastNode.next:
-            prevNode = slowNode
-            slowNode = slowNode.next
-            fastNode = fastNode.next.next
+        return l
+    
+    def convert(self, l, r) -> Optional[TreeNode]:
+        if l > r:
+            return None
         
-        if prevNode:
-            prevNode.next = None
+        mid = (l+r) // 2
+        left = self.convert(l, mid-1)
+        root = TreeNode(self.head.val)
+        root.left = left
+        self.head = self.head.next
+        root.right = self.convert(mid+1, r)
         
-        return slowNode
+        return root
         
         
     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
-        if not head:
-            return None
+        l = self.size(head)
+        self.head = head
         
-        mid = self.findMid(head)
-        
-        root = TreeNode(mid.val)
-        
-        if mid == head:
-            return root
-        
-        root.left = self.sortedListToBST(head)
-        root.right = self.sortedListToBST(mid.next)
+        root = self.convert(0, l-1)
         
         return root
+    
+    
         
