6aee647ff04c257e332e48c24a6534c0038e0954
Sejin Park <42539506+sejin-P@users.noreply.github.com>
2024-10-13 00:29:51 +0900

    Time: 76 ms (97.59%), Space: 21.1 MB (70.16%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        cnt = defaultdict(int)
+        for num in nums:
+            cnt[num] += 1
+        
+        values = cnt.values()
+        min_val = min(values)
+        max_val = max(values)
+        
+        c = [0]*(max_val-min_val+1)
+
+        val_dict = defaultdict(list)
+        for key in cnt.keys():
+            c[cnt[key]-min_val] += 1
+            val_dict[cnt[key]-min_val].append(key)
+       
+        res = []
+        for i in range(len(c)-1,-1,-1):
+            k -= c[i]
+            if c[i] != 0:
+                res += val_dict[i]
+            if k == 0:
+                return res
+        
