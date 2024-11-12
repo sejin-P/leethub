3fdf7b73d11bf1532fc425f82f43569967d0a2d4
Sejin Park <42539506+sejin-P@users.noreply.github.com>
2024-11-12 23:52:10 +0900

    Time: 27 ms (96.91%), Space: 29.1 MB (45.90%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,16 +1,42 @@
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        dictionary.sort(key=lambda x : len(x))
+        trie = {}
+        
+        for word in dictionary:
+            prev = trie
+            for letter in word:
+                if letter not in prev:
+                    prev[letter] = {}
+                prev = prev[letter]
+            prev[''] = ''
+        
         splitted = sentence.split()
         
         for i in range(len(splitted)):
-            for root in dictionary:
-                if self.isDerivative(splitted[i], root):
-                    splitted[i] = root
-                    break
+            root = self.derivativeWord(trie, splitted[i])
+            if root != '':
+                splitted[i] = root
         
         return ' '.join(splitted)
         
-    def isDerivative(self, word, root):
-        return word[:min(len(word), len(root))] == root
+    
+    def derivativeWord(self, trie, word):
+        prev = trie
+        root = ''
+        for letter in word:
+            if letter not in prev:
+                if '' in prev and prev[''] == '':
+                    return root
+                return ''
+            prev = prev[letter]
+            root += letter
+            if '' in prev and prev[''] == '':
+                return root
+        
+        if '' in prev and prev[''] == '':
+            return root
+        
+        return ''
+        
+        
         
