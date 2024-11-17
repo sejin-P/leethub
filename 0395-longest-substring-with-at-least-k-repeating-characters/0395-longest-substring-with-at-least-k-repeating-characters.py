fe6f6ffd824411b1aa55c4356da336dcc5494e47
Sejin Park <42539506+sejin-P@users.noreply.github.com>
2024-11-17 22:54:03 +0900

    Time: 37 ms (58.04%), Space: 16.8 MB (38.70%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
@@ -1,26 +1,38 @@
+from collections import Counter
+
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
         asciia = ord('a')
+        stringCounter = Counter(s)
         
+        maxUnique = len(stringCounter.keys())
         res = 0
         
-        stringCnt = [0]*26
-        for i in range(len(s)):
-            stringCnt[ord(s[i]) - asciia] += 1
-            if self.isValid(stringCnt, k):
-                res = max(res, i+1)
-                    
-            copied = [stringCnt[l] for l in range(26)]
-            for j in range(i):
-                copied[ord(s[j]) - asciia] -= 1
-                if self.isValid(copied, k):
-                    res = max(res, i-j)
+        for currUnique in range(1, maxUnique+1):
+            start, end = 0, 0
+            unique, countAtLeastK = 0, 0
+            stringCnt = [0]*26
+            
+            while end < len(s):
+                if unique <= currUnique:
+                    order = ord(s[end]) - asciia
+                    if stringCnt[order] == 0:
+                        unique += 1
+                    stringCnt[order] += 1
+                    if stringCnt[order] == k:
+                        countAtLeastK += 1
+                    end += 1
+                else:
+                    order = ord(s[start]) - asciia
+                    if stringCnt[order] == 1:
+                        unique -= 1
+                    stringCnt[order] -= 1
+                    if stringCnt[order] == k-1:
+                        countAtLeastK -= 1
+                    start += 1
+                if (unique == currUnique) and (unique == countAtLeastK):
+                    res = max(res, end-start)
         
         return res
-    
-    def isValid(self, stringCnt, k):
-        for cnt in stringCnt:
-            if cnt != 0 and cnt < k:
-                return False
-        return True
+        
         
