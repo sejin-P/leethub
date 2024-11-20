e974cfa716225ac2b0cf328b45a675404c5fd552
Sejin Park <42539506+sejin-P@users.noreply.github.com>
2024-11-21 00:33:32 +0900

    Time: 967 ms (14.88%), Space: 16.5 MB (88.89%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        i = 0
+        
+        while True:
+            if i*i > x:
+                return i-1
+            i += 1
+        
