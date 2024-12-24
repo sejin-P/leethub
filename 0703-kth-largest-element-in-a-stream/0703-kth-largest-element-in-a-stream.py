c4c48a7bb16af5b764cfc40fde3db55bfc2b126c
Sejin Park <42539506+sejin-P@users.noreply.github.com>
2024-12-24 23:05:01 +0900

    Time: 27 ms (22.40%), Space: 23.9 MB (6.90%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -1,37 +1,19 @@
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        nums.sort(reverse=True)
-        self.nums = nums
+        self.heap = []
         self.k = k
+        
+        for num in nums:
+            self.add(num)
 
     def add(self, val: int) -> int:
-        if len(self.nums) >= self.k and val < self.nums[self.k-1]:
-            return self.nums[self.k-1]
+        if len(self.heap) < self.k or self.heap[0] < val:
+            heapq.heappush(self.heap, val)
+            if len(self.heap) > self.k:
+                heapq.heappop(self.heap)
         
-        if len(self.nums) == 0:
-            self.nums.append(val)
-            return val
-        
-        l, r = 0, min(self.k-1, len(self.nums)-1)
-        
-        while l < r:
-            mid = (l+r) // 2
-            if self.nums[mid] > val:
-                l = mid+1
-                continue
-            if self.nums[mid] < val:
-                r = mid
-                continue
-            break
-        mid = (l+r) // 2
-        
-        if self.nums[mid] >= val:  
-            self.nums.insert(mid+1, val)
-        else:
-            self.nums.insert(mid, val)
-        
-        return self.nums[self.k-1]
+        return self.heap[0]
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
